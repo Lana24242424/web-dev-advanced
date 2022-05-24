3c37c919c38f8bd4ecd813c46dd2d16d929c3dd8
Svetlana <124veg@gmail.com>
2022-05-24 11:58:49 +1000

    innerHTML

--- app.js
+++ app.js
@@ -39,3 +39,9 @@ firstH1Element.parentElement.removeChild(firstH1Element);
 let firstParagraph = document.querySelector(".first-paragraph");
 
 secondParagraph.parentElement.append(firstParagraph);
+
+//innerHTML
+
+console.log(firstParagraph.innerHTML);
+
+firstParagraph.innerHTML = "Hi! This is <strong>important!</strong>";
