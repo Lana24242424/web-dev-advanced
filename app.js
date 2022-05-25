372a66f41b4896d8e793fa850dced1d77c8aaf62
Svetlana <124veg@gmail.com>
2022-05-25 12:32:40 +1000

    input

--- app.js
+++ app.js
@@ -4,4 +4,17 @@ function changeParagraphText() {
     paragraphElement.textContent = "Clicked!";
 }
 
-paragraphElement.addEventListener("click", changeParagraphText);
+paragraphElement.addEventListener("click", changeParagraphText); 
+
+
+let inputElement = document.querySelector("input");
+
+function retrieveUserInput(event) {
+//let enteredText = inputElement.value;
+let enteredText = event.target.value;
+// let enteredText = event.data; => This outputs letters one by one!
+console.log(enteredText);
+//console.log(event);
+}
+
+inputElement.addEventListener("input", retrieveUserInput);
