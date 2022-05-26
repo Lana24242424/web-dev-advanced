57c33135115b0624c32b13f2ae01a5e1774a8b08
Svetlana <124veg@gmail.com>
2022-05-26 23:59:37 +1000

    added event listeners and functions to the exersise

--- exercise-2/exercise.js
+++ exercise-2/exercise.js
@@ -2,15 +2,32 @@
 
 // 1) Select the two <button> elements and store them in two different variables.
 //    - Select the first button without adding or using any "id"
+
+const buttonOne = document.querySelector("button");
+
 //    - Select the second button by using an "id"
+
+const buttonTwo = document.getElementById("second-button");
+
 // 2) Add "click" event listener to both buttons (with two different functions).
 //    The functions should "console.dir()" the clicked buttons.
 //    - Output the first button by using the variable in which it's stored
 //    - Output the second button WITHOUT using the variable in which it's stored
+
+function removeParagraph() {
+console.dir(buttonOne);
+}
+
+function changeBackgroundColor(event) {
+console.dir(event.target);
+}
+
+buttonOne.addEventListener("click", removeParagraph);
+buttonTwo.addEventListener("click", changeBackgroundColor);
 // 3) Now select and store the paragraphs mentioned in the text you see on the page
 //    (first and third paragraph)
 //    - Select BOTH paragraphs by drilling into the document and "navigating" to the
-//      mentioned elements
+//      mentioned elements 
 //    - If you struggle with DOM drilling, use "ids" instead but watch the solution!
 // 4) Change the functions from (2) such that:
 //    - The first button removes the third paragraph (i.e. the <p> prior to it)
