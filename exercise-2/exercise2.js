27519c188433503a4653bbd0a415a3f1059051ba
Svetlana <124veg@gmail.com>
2022-05-27 21:05:07 +1000

    exersise

--- exercise-2/exercise2.js
+++ exercise-2/exercise2.js
@@ -14,23 +14,45 @@ const buttonTwo = document.getElementById("second-button");
 //    - Output the first button by using the variable in which it's stored
 //    - Output the second button WITHOUT using the variable in which it's stored
 
-function removeParagraph() {
-console.dir(buttonOne);
-}
+//function removeParagraph() {
+//console.dir(buttonOne);
+//}
 
-function changeBackgroundColor(event) {
-console.dir(event.target);
-}
+//function changeBackgroundColor(event) {
+//console.dir(event.target);
+//}
 
-buttonOne.addEventListener("click", removeParagraph);
-buttonTwo.addEventListener("click", changeBackgroundColor);
+//buttonOne.addEventListener("click", removeParagraph);
+//buttonTwo.addEventListener("click", changeBackgroundColor);
+
 // 3) Now select and store the paragraphs mentioned in the text you see on the page
 //    (first and third paragraph)
 //    - Select BOTH paragraphs by drilling into the document and "navigating" to the
 //      mentioned elements 
+const firstParagraph = document.body.children[2].children[1];
+console.dir(firstParagraph);
+
+const thirdParagraph = document.body.children[2].children[3];
+console.dir(thirdParagraph);
+
 //    - If you struggle with DOM drilling, use "ids" instead but watch the solution!
 // 4) Change the functions from (2) such that:
 //    - The first button removes the third paragraph (i.e. the <p> prior to it)
 //    - The second button changes the background color of the first paragraph to blue
+
+
+function removeParagraph() {
+thirdParagraph.remove;
+}
+
+function changeBackgroundColor(event) {
+firstParagraph.style.backgroundColor = "blue";
+}
+
+buttonOne.addEventListener("click", removeParagraph);
+buttonTwo.addEventListener("click", changeBackgroundColor);
+
+
+
 // 5) Solve (4) both by changing the "inline styles" as well as by adding CSS classes
 //    Note: You'll have to add those classes to the styles.css file first!
